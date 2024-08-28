4ca2437c2df08d48c0b80e06ed9b0b782adea576
DikshitDarji <darjidikshit30@gmail.com>
2024-08-28 11:08:26 +0530

    javascript

--- JavaScript/Assignment-1/que1.js
+++ JavaScript/Assignment-1/que1.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 function sayHello() {
   console.log("Hello, World");
 }
 sayHello();
-=======
 function sayHello() {
   console.log("Hello, World");
 }
 sayHello();
->>>>>>> 89c6cba099f00bd850db86c209a32313ae03678b
